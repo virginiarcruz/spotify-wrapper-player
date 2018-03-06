0679eef46191ab9c1630c659e56e7055d0ef08d4
Virginia Cruz <virginiarcruz@gmail.com>
2018-03-06 14:40:29 -0300

    adicionando a lista de albums no player

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQAw8etrCmTBeCYz5qOAbrgFdfmDkIYSCJqSSxfCcqZ-ZU78tOIPQ5CNfnR8TDoREFRegg4o8XDP8md06SO5sICqfn_Tm9TAZ1r3roSenumHBRkysEaURYfXNZpTkAdWYBUbDlT5l98FXu4'
+});
+
+export default spotify;
